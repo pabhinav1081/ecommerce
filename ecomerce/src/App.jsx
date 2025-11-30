c9f997909b4dbb15b1fbe552cf674eeec3f217e5
pabhinav1081 <abhinavpandey2405@gmail.com>
2025-11-30 12:28:18 +0530

    complete with all page

--- ecomerce/src/App.jsx
+++ ecomerce/src/App.jsx
@@ -3,13 +3,14 @@ import './App.css'
 import OrdersPage from './pages/OrdersPage.jsx'
 import Checkoutpage from './pages/Checkoutpage.jsx'
 import HomePage from './pages/HomePage.jsx'
-
+import Tracking from './pages/tracking.jsx'
 function App() {
   return (
     <Routes>
       <Route path='/' element={<HomePage />} />
       <Route path ="/checkout" element={<Checkoutpage />} />
       <Route path ="/order" element={<OrdersPage />} />
+      <Route path ="/tracking" element={<Tracking />} />
 
     </Routes>
     //<HomePage />
