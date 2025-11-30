6721465c38239a33c47278008bdc38cca3938db2
pabhinav1081 <abhinavpandey2405@gmail.com>
2025-11-30 12:16:14 +0530

    updated links to match router

--- ecomerce/src/App.jsx
+++ ecomerce/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <Routes>
       <Route path='/' element={<HomePage />} />
-      <Route path ="checkout.html" element={<Checkoutpage />} />
+      <Route path ="/checkout" element={<Checkoutpage />} />
     </Routes>
     //<HomePage />
 
