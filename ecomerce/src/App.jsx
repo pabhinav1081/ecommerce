a00c80cf759fa1bb875fce977986c475a166f857
pabhinav1081 <abhinavpandey2405@gmail.com>
2025-11-30 12:25:49 +0530

    add order page

--- ecomerce/src/App.jsx
+++ ecomerce/src/App.jsx
@@ -1,5 +1,6 @@
 import {Routes, Route} from 'react-router'
 import './App.css'
+import OrdersPage from './pages/OrdersPage.jsx'
 import Checkoutpage from './pages/Checkoutpage.jsx'
 import HomePage from './pages/HomePage.jsx'
 
@@ -8,6 +9,8 @@ function App() {
     <Routes>
       <Route path='/' element={<HomePage />} />
       <Route path ="/checkout" element={<Checkoutpage />} />
+      <Route path ="/order" element={<OrdersPage />} />
+
     </Routes>
     //<HomePage />
 
